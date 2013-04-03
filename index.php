8fb3e27d9539dee3a7e59cc9a227f1b77dc94c5a
Lisa Sy <lsy@wesleyan.edu>
2013-04-03 10:47:11 -0400

    make live

--- index.php
+++ index.php
@@ -1,16 +1,126 @@
-<?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  return "dbname=d43s3f9anensdc host=ec2-107-21-107-194.compute-1.amazonaws.com port=5432 user=fyvifrpwiqyqmm password=Vdmf__3W3zLNVY7e9G7Geh7bn3 sslmode=require";
-}
- 
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-   echo "Database connection error."
-   exit;
-}
- 
-$result = pg_query($db, "SELECT statement goes here");
-?>
+<?php include 'header.php' ?>
+  <body>     
+<div class="navbar navbar-static-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <div class="row">
+                <div class="span2">
+                    <a class="brand" href="index.php"><img src="img/logo-white.png"></a>
+                </div>
+                <div class="span5 offset3">
+                    <div class="pull-right">
+                    <ul class="nav landing-nav">
+                      <li class="active"><a href="#">Home</a></li>
+                      <li><a href="info-about.php">About</a></li>
+                      <li><a href="features.php">Features</a></li>
+                      <li><a href="#">Log In</a></li>
+                      <li><a href="#" class="btn btn-green">Sign Up</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="body-content" class="landing">
+
+<div id="landing-masthead">
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Textbooks made easy.</h1>
+            <div class="info">
+                <section class="span5">
+                    <img src="http://placehold.it/600x350">
+                </section>
+                <section class="span5 offset1">
+                    <p class="lead">Texts.com allows students to buy and sell textbooks to one another on their campuses. Stop overpaying, sign up now.</p>
+                    <a class="btn facebook">Sign up with Facebook</a>
+                    <p><small><a href="#" data-toggle="tooltip" title="first tooltip">hover over me</a></small></p>
+<!--                     <p><small>or</small></p>
+                    <a href="#">Sign up with your email address</a> -->
+                </section>
+            </div>
+        </div>
+    <div class="row learn-more">
+        <div class="span12">
+            <a href="#marketing" class="btn btn-green btn-large learn-more-btn">Learn more</a>
+        </div>
+    </div>
+    </div>
+</div>
+
+<div id="marketing" class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="span4">
+          <img class="img-circle" data-src="holder.js/140x140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <img class="img-circle" data-src="holder.js/140x140">
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <img class="img-circle" data-src="holder.js/140x140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+      </div><!-- /.row -->
+
+
+<div id="landing-features">
+  <!-- START THE FEATURETTES -->
+<hr class="featurette-divider">
+
+  <div class="featurette">
+    <img class="featurette-image pull-right" src="http://placehold.it/600x350">
+    <h2 class="featurette-heading">First featurette headling. <span class="muted">It'll blow your mind.</span></h2>
+    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <div class="featurette">
+    <img class="featurette-image pull-left" src="http://placehold.it/600x350">
+    <h2 class="featurette-heading">Oh yeah, it's that good. <span class="muted">See for yourself.</span></h2>
+    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <div class="featurette">
+    <img class="featurette-image pull-right" src="http://placehold.it/600x350">
+    <h2 class="featurette-heading">And lastly, this one. <span class="muted">Checkmate.</span></h2>
+    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    <a class="btn">Sign up now</a>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <!-- /END THE FEATURETTES -->    
+</div>
+
+<div class="container">
+    <section class="market-preview">
+        <h2>It's all on campus.</h2>
+        <p>Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <a class="btn">Learn more</a>
+    </section>
+</div>
+
+</div>
+</div>
+
+
+<?php include 'footer.php' ?>
+<?php include 'javascript.php' ?>
+
+  </body>
+</html>
--- landing.php
+++ landing.php
@@ -44,14 +44,40 @@
         </div>
     <div class="row learn-more">
         <div class="span12">
-            <a href="#landing-features" class="btn btn-green btn-large learn-more-btn">Learn more</a>
+            <a href="#marketing" class="btn btn-green btn-large learn-more-btn">Learn more</a>
         </div>
     </div>
     </div>
 </div>
 
-<div id="landing-features" class="container">
+<div id="marketing" class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="span4">
+          <img class="img-circle" data-src="holder.js/140x140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <img class="img-circle" data-src="holder.js/140x140">
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <img class="img-circle" data-src="holder.js/140x140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+      </div><!-- /.row -->
+
+
+<div id="landing-features">
   <!-- START THE FEATURETTES -->
+<hr class="featurette-divider">
 
   <div class="featurette">
     <img class="featurette-image pull-right" src="http://placehold.it/600x350">
@@ -89,6 +115,7 @@
     </section>
 </div>
 
+</div>
 </div>
 
 
