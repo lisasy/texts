996f2164a41da2991c9a33e32d2578eb1d3879d6
Lisa Sy <lsy@wesleyan.edu>
2013-04-06 16:00:59 -0400

    added 404.php

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php include 'header.php' ?>
+<body>     
+<?php include 'navbar.php' ?>
+
+
+<div class="body-content">
+
+<div class="container">
+  <div class="row">
+    <section class="error span8 offset1">
+            <h1>I'm sorry, the page you're looking for does not exist!</h1>
+            <h3>Find your way back...</h3>
+            <nav>
+                <ul>
+                    <li><a href="" class="btn btn-green btn-large">Find books to buy</a></li>
+                    <li><a href="" class="btn btn-green btn-large">Find books to sell</a></li>
+                    <li><a href="" class="btn btn-tan btn-large">View my profile</a></li>
+                </ul>
+            </nav>
+    </section>
+</div>
+</div>
+
+</div>
+
+<?php include 'footer.php' ?>
+<?php include 'javascript.php' ?>
+
+</body>
+</html>
