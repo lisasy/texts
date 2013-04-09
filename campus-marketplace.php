6fb6218045f36ca88916b9263b8ccd7d57750939
Lisa Sy <lsy@wesleyan.edu>
2013-04-09 03:05:51 -0400

    campus marketplace is good to go, for now

--- campus-marketplace.php
+++ campus-marketplace.php
@@ -1,6 +1,6 @@
 <?php include 'header.php' ?>
 <body>     
-<?php include 'navbar-unlogged.php' ?>
+<?php include 'navbar.php' ?>
 
 
 <div id="campus-marketplace" class="body-content">
@@ -12,18 +12,18 @@
 	  		<div class="bar" style="width: 40%"></div>
 		</div>
 	  	<h2>40% of Wesleyan University are on Texts.com</h2>
-	  	<h4>More students mean better prices.</h4>
+	  	<h4>More students mean no middleman means better prices.</h4>
 	  	<a href="" class="btn btn-green">Invite your friends now!</a>
   	</header>
 </div>
 
-<div id="campus-banner" class="container">
+<!-- <div id="campus-banner" class="container">
 	<section class="">
 		<h1>More students</h1>
 		<h1>mean no middleman</h1>
 		<h1>means better prices.</h1>
 	</section>	
-</div>
+</div> -->
 
 <div id="campus-analytics">
 	<div class="container">
@@ -49,6 +49,36 @@
 					</li>
 				</ul>
 			</section>
+		</div>
+		<div class="row">
+			<section class="span5 offset1">
+				<ol>
+					<h3>Most Demanded Books</h3>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Architecture of Mughal India by John Doe</a></li>
+					<li><a href="#">Clients</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ol>
+			</section>
+			<section class="span5 offset1">
+				<ol>
+					<h3>Most Owned Books</h3>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Modern China by Jane Doe</a></li>
+					<li><a href="#">The Perks of Being a Wallflower by Stephen Chbosky</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ol>
+			</section>
+		</div>
+		<div class="row">
+			<div id="find-action">
+				<nav>
+					<ul>
+						<li><a class="btn-green btn">Find books to sell</a></li>
+						<li><a class="btn-green btn">Find books to buy</a></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	</div>
 </div>
